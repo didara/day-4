32d9c8b4df8ac144f2c764446cf4b687ca899e92
Didara Pernebayeva <didarapernebayeva@Macintosh.local>
2015-06-17 14:02:09 +0600

    Collection View design created

--- Day 4 tutorial/PhotoCollectionViewCell.h
+++ Day 4 tutorial/PhotoCollectionViewCell.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface PhotoCollectionViewCell : UICollectionViewCell
+@property (nonatomic) UIImageView *imageView;
 
 @end
--- Day 4 tutorial/ViewController.m
+++ Day 4 tutorial/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "PhotoCollectionViewCell.h"
+
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -16,12 +19,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    [self.collectionView registerClass:[PhotoCollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+#pragma mark - UICollectionView methods;
+
+- (NSInteger) numberOfSectionsInCollectionView:(UICollectionView *)collectionView;
+{
+    return 1;
+}
+
+-(NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+}
+-(UICollectionViewCell *) collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
+        forIndexPath:indexPath];
+    cell.imageView.image = [UIImage imageNamed:@"lookImage.jpeg"];
+    return cell;
+}
+
+
+
 
 @end
+
