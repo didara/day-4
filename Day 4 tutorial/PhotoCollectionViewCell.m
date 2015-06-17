e63addc531e31e6aaabefc19f72a35e0d98f753f
Didara Pernebayeva <didarapernebayeva@Macintosh.local>
2015-06-17 12:34:24 +0600

    first commit

---
+++ Day 4 tutorial/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  Day 4 tutorial
+//
+//  Created by Didara Pernebayeva on 17.06.15.
+//  Copyright (c) 2015 Didara Pernebayeva. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
